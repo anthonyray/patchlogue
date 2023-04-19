9e2020ccdeebe5e0cb54ca2e4788e57db1d81b82
Anthony Reinette <anthonyray@users.noreply.github.com>
2023-04-19 17:33:26 +0200

    adds style.css

--- ci-checks/ci.sh
+++ ci-checks/ci.sh
@@ -5,7 +5,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
-
+# rust
 echo "patchlogue CI"
 echo "Running formatter"
 cargo fmt --check --quiet
@@ -13,4 +13,8 @@ echo "Running linter"
 cargo clippy
 echo "Running tests"
 cargo test
+# www
+echo "Running formatting for www"
+( cd www ; npx prettier --check *.html )
+( cd www ; npx prettier --check *.css )
 echo "done"
